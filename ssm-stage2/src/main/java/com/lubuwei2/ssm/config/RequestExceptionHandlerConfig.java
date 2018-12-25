24f026e530a93f66eefe9d6402ec15b70e3572ad
linyaqi <mr03web@foxmail.com>
2018-12-25 17:00:00 +0800

    mapper

---
+++ ssm-stage2/src/main/java/com/lubuwei2/ssm/config/RequestExceptionHandlerConfig.java
@@ -0,0 +1,24 @@
+package com.lubuwei2.ssm.config;
+
+import com.lubuwei2.ssm.api.Api;
+import com.lubuwei2.ssm.api.ApiGenerator;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2018/3/23.
+ */
+
+@RestControllerAdvice
+public class RequestExceptionHandlerConfig {
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public Api<String> MethodArgumentNotValidHandler(HttpServletRequest request, MethodArgumentNotValidException exception)
+    {
+        // 按需重新封装需要返回的错误信息
+        String msg = exception.getBindingResult().getFieldError().getDefaultMessage();
+        return ApiGenerator.fail(msg);
+    }
+}
