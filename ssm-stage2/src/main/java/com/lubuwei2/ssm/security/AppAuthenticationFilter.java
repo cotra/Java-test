dd3dc3362d75c339238d16ccd083d8f266f82e70
linyaqi <mr03web@foxmail.com>
2018-12-28 15:05:48 +0800

    security test

--- ssm-stage2/src/main/java/com/lubuwei2/ssm/security/AppAuthenticationFilter.java
+++ ssm-stage2/src/main/java/com/lubuwei2/ssm/security/AppAuthenticationFilter.java
@@ -1,10 +1,8 @@
 package com.lubuwei2.ssm.security;
 
-import com.lubuwei2.ssm.entity.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
