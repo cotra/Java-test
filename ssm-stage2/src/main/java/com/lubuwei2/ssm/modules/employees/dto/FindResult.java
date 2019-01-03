69a5a2bcb6835bb20d494204994cbaead81c99cc
linyaqi <mr03web@foxmail.com>
2019-01-03 16:01:58 +0800

    雇员功能

---
+++ ssm-stage2/src/main/java/com/lubuwei2/ssm/modules/employees/dto/FindResult.java
@@ -0,0 +1,70 @@
+package com.lubuwei2.ssm.modules.employees.dto;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class FindResult {
+    private Long empNo;
+    private Date birthDate;
+    private String FirstName;
+    private String LastName;
+    private Integer gender;
+    private Date hireDate;
+    private Long eid;
+
+    public Long getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(Long empNo) {
+        this.empNo = empNo;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Long getEid() {
+        return eid;
+    }
+
+    public void setEid(Long eid) {
+        this.eid = eid;
+    }
+}
---
+++ ssm-stage2/src/main/java/com/lubuwei2/ssm/modules/employees/dto/Flag.java
@@ -0,0 +1,6 @@
+package com.lubuwei2.ssm.modules.employees.dto;
+
+public class Flag {
+    // 成功
+    public static final Integer OK = 0;
+}
